042e2925b73b7c36676bdc0caf7a339a2b6243a2
g.babaeva <g.babaeva@fun-box.ru>
2020-11-18 17:15:25 +0700

    [MOD-1] Обновление компоненты App для редиректов на страницы.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import Header from '../src/app/components/header';
+import WelcomePage from '../src/app/views/welcome-page';
+import MapPage from '../src/app/views/map-page';
+import ProfilePage from '../src/app/views/profile-page';
 
 class App extends React.Component {
+    state = {
+        page: 'welcome-page',
+    };
+
     render() {
-      return (
-        <div className="container">
-            {/*  Заглушка   */}
-        </div>
-      );
+        const { page } = this.state;
+
+        return (
+            <div className="container">
+                <Header/>
+                <main className="main">
+                    {
+                        {
+                            'welcome-page': <WelcomePage/>,
+                            'map-page': <MapPage/>,
+                            'profile-page': <ProfilePage/>
+                        }[page]
+                    }
+                </main>
+            </div>
+        );
     };
 }
 
