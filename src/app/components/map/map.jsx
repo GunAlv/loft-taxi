11612ef84edfb32d82714af0403a7e15ba924e54
g.babaeva <g.babaeva@fun-box.ru>
2020-11-24 17:10:26 +0700

    [MOD-2] Инициализация карты.

--- src/app/components/map/map.jsx
+++ src/app/components/map/map.jsx
@@ -1,10 +1,54 @@
 import React from 'react';
+import MapBox from '../map-box';
+import mapboxgl from 'mapbox-gl';
+import mapData from '../../common/constants/map';
+
 import getClass from '../../utils/getClass';
 
-const Map = (props) => (
-    <div className={getClass("map", props)}>
-        <h1>Карта</h1>
-    </div>
-);
+mapboxgl.accessToken = mapData.accessToken;
+
+class Map extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = mapData.coords;
+
+        this.mapContainer = undefined;
+        this.initializedMap = undefined;
+    };
+
+    componentDidMount() {
+        this.initMap();
+    };
+
+    initMap = () => {
+        this.initializedMap = new mapboxgl.Map({
+            container: this.mapContainer,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [this.state.lng, this.state.lat],
+            zoom: this.state.zoom
+        });
+
+        if (navigator.geolocation) {
+            const setCoords = (position) => {
+                this.setState({
+                    lng: position.coords.longitude,
+                    lat: position.coords.latitude,
+                });
+
+                this.initializedMap.setCenter([position.coords.longitude, position.coords.latitude]);
+            };
+
+            navigator.geolocation.getCurrentPosition(setCoords);
+        }
+    };
+
+    render() {
+        return (
+            <div className={getClass("map", this.props)}>
+                <MapBox ref={el => this.mapContainer = el} />
+            </div>
+        );
+    };
+}
 
 export default Map;
