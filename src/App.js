5d8b510de3813a6f988834eb1755efc271be5461
g.babaeva <g.babaeva@fun-box.ru>
2020-11-24 18:13:13 +0700

    [MOD-2] Добавление общей темы.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import { ThemeProvider } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import AuthProvider from '../src/app/common/providers/auth-provider';
 import MainApp from '../src/app/components/main-app';
+
+const theme = createMuiTheme({
+    overrides: {
+        MuiCssBaseline: {
+            '@global': {
+                body: {
+                    fontSize: '18px',
+                    lineHeight: '21px',
+                    letterSpacing: 0,
+                    color: '#000',
+                    backgroundColor: '#fff',
+                },
+            },
+        },
+    },
+});
 
 class App extends React.Component {
     render() {
         return (
-            <AuthProvider>
-                <MainApp/>
-            </AuthProvider>
+            <ThemeProvider theme={theme}>
+                <CssBaseline>
+                    <AuthProvider>
+                        <MainApp/>
+                    </AuthProvider>
+                </CssBaseline>
+            </ThemeProvider>
         );
     };
 }
