454bc740a7bd7753041b54f98f60b69fa6705aa3
g.babaeva <g.babaeva@fun-box.ru>
2020-12-03 15:37:24 +0700

    [MOD-3] Удаление данных карты из localStorage после успешной регистрации.

--- src/app/common/utils/paymentStorage.js
+++ src/app/common/utils/paymentStorage.js
@@ -10,3 +10,9 @@ export const getFromPaymentStorage = (value) => {
     const storage = JSON.parse(localStorage.getItem(LOFT_TAXI_PAYMENT_STORAGE));
     return storage ? storage[value] : undefined;
 };
+
+export const deletePaymentStorage = () => {
+    const storage = JSON.parse(localStorage.getItem(LOFT_TAXI_PAYMENT_STORAGE));
+    console.log(storage)
+    if (storage) return localStorage.removeItem(LOFT_TAXI_PAYMENT_STORAGE);
+};
--- src/app/module/middlewares/registerMiddleware.js
+++ src/app/module/middlewares/registerMiddleware.js
@@ -4,6 +4,8 @@ import { SET_REGISTER } from '../../common/constants/action-types';
 import { getAuthProgress, getAuthStatus } from '../actions/auth';
 import { setAuthStorage } from '../../common/utils/authStorage';
 import { getRegisterError } from '../actions/register';
+import { deletePaymentStorage } from '../../common/utils/paymentStorage';
+import { getPaymentData } from '../actions/payment';
 
 export const registerMiddleware = store => next => action => {
     if (action.type === SET_REGISTER) {
@@ -21,6 +23,13 @@ export const registerMiddleware = store => next => action => {
                     return;
                 }
 
+                deletePaymentStorage();
+                store.dispatch(getPaymentData({
+                    cardNumber: '',
+                    cardName: '',
+                    expiryDate: '',
+                    cvc: '',
+                }));
                 setAuthStorage(true, data.token);
                 store.dispatch(getRegisterError(''));
                 store.dispatch(getAuthStatus(true, data.token));
