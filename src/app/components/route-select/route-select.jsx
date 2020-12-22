d1004b9042e447ed01e5a5157c1546812fe4ae70
g.babaeva <g.babaeva@fun-box.ru>
2020-12-22 22:56:41 +0700

    [MOD-5] Переписывание формы вызова такси.

--- src/app/components/route-select/route-select.jsx
+++ src/app/components/route-select/route-select.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { FormContainer } from '../form/style';
@@ -6,114 +6,133 @@ import { RouteSelectContainer, RouteSelectControl } from './style';
 import NativeSelect from '@material-ui/core/NativeSelect';
 import Button from '@material-ui/core/Button';
 import { setAddress, setRoute } from '../../module/actions/map';
+import { Controller, useForm } from 'react-hook-form';
 
-class RouteSelect extends React.Component {
-    state = {
+const RouteSelect = ({ setAddress, addressList, setRoute }) => {
+    const [state, setState] = useState({
         addressFrom: '',
         addressTo: '',
-    };
+    });
 
-    componentDidMount() {
-        const { setAddress } = this.props;
+    const methods = useForm({
+        mode: 'onChange',
+    });
+
+    const { handleSubmit, control, formState } = methods;
+
+    useEffect(() => {
         setAddress();
-    }
+    }, []);
 
-    handleChangeFrom = (event) => {
-        this.setState({ addressFrom: event.target.value });
+    const handleChangeFrom = (event) => {
+        setState(prevState => ({
+            ...prevState,
+            addressFrom: event.target.value,
+        }));
     };
 
-    handleChangeTo = (event) => {
-        this.setState({ addressTo: event.target.value });
+    const handleChangeTo = (event) => {
+        setState(prevState => ({
+            ...prevState,
+            addressTo: event.target.value,
+        }));
     }
 
-    submit = (e) => {
-        e.preventDefault();
-
-        const { setRoute } = this.props;
-        setRoute(this.state.addressFrom, this.state.addressTo);
+    const submit = (data) => {
+        setRoute(data.addressFrom, data.addressTo);
     }
 
-    render() {
-        const { addressFrom, addressTo } = this.state;
-        const { addressList } = this.props;
-
-        const isDisabled = (!addressFrom.length || !addressTo.length);
-
-        return (
-            <RouteSelectContainer
-                data-testid="route-select"
-            >
-                <FormContainer
-                    onSubmit={this.submit}
+    return (
+        <RouteSelectContainer
+            data-testid="route-select"
+        >
+            <FormContainer onSubmit={handleSubmit(submit)}>
+                <RouteSelectControl>
+                    <Controller
+                        control={control}
+                        name="addressFrom"
+                        rules={{ required: true }}
+                        render={({ onChange }) => (
+                            <NativeSelect
+                                value={state.addressFrom}
+                                inputProps={{
+                                    'aria-label': 'addressFrom',
+                                    'data-testid': 'select-from',
+                                }}
+                                onChange={(e) => {
+                                    onChange(e);
+                                    handleChangeFrom(e);
+                                }}
+                            >
+                                <option
+                                    value=""
+                                    disabled
+                                >
+                                    Откуда
+                                </option>
+                                {addressList.map(address => (
+                                    <option
+                                        data-testid="select-option-from"
+                                        key={address}
+                                        value={address}
+                                        disabled={address === state.addressTo}
+                                    >
+                                        {address}
+                                    </option>
+                                ))}
+                            </NativeSelect>
+                        )}
+                    />
+                </RouteSelectControl>
+                <RouteSelectControl>
+                    <Controller
+                        control={control}
+                        name="addressTo"
+                        rules={{ required: true }}
+                        render={({ onChange }) => (
+                            <NativeSelect
+                                value={state.addressTo}
+                                name="addressTo"
+                                inputProps={{
+                                    'aria-label': 'addressTo',
+                                    'data-testid': 'select-to',
+                                }}
+                                onChange={(e) => {
+                                    onChange(e);
+                                    handleChangeTo(e);
+                                }}
+                            >
+                                <option
+                                    value=""
+                                    disabled
+                                >
+                                    Куда
+                                </option>
+                                {addressList.map(address => (
+                                    <option
+                                        data-testid="select-option-to"
+                                        key={address}
+                                        value={address}
+                                        disabled={address === state.addressFrom}
+                                    >
+                                        {address}
+                                    </option>
+                                ))}
+                            </NativeSelect>
+                        )}
+                    />
+                </RouteSelectControl>
+                <Button
+                    data-testid="route-select-button"
+                    variant="contained"
+                    type="submit"
+                    disabled={!formState.isValid}
                 >
-                    <RouteSelectControl>
-                        <NativeSelect
-                            value={addressFrom}
-                            name="addressFrom"
-                            inputProps={{
-                                'aria-label': 'addressFrom',
-                                'data-testid': 'select-from',
-                            }}
-                            onChange={this.handleChangeFrom}
-                        >
-                            <option
-                                value=""
-                                disabled
-                            >
-                                Откуда
-                            </option>
-                            {addressList.map(address => (
-                                <option
-                                    data-testid="select-option-from"
-                                    key={address}
-                                    value={address}
-                                    disabled={address === addressTo}
-                                >
-                                    {address}
-                                </option>
-                            ))}
-                        </NativeSelect>
-                    </RouteSelectControl>
-                    <RouteSelectControl>
-                        <NativeSelect
-                            value={addressTo}
-                            name="addressTo"
-                            inputProps={{
-                                'aria-label': 'addressTo',
-                                'data-testid': 'select-to',
-                            }}
-                            onChange={this.handleChangeTo}
-                        >
-                            <option
-                                value=""
-                                disabled
-                            >
-                                Куда
-                            </option>
-                            {addressList.map(address => (
-                                <option
-                                    data-testid="select-option-to"
-                                    key={address}
-                                    value={address}
-                                    disabled={address === addressFrom}
-                                >
-                                    {address}
-                                </option>
-                            ))}
-                        </NativeSelect>
-                    </RouteSelectControl>
-                    <Button
-                        data-testid="route-select-button"
-                        variant="contained"
-                        type="submit"
-                        disabled={isDisabled}
-                    >
-                        Заказать
-                    </Button>
-                </FormContainer>
-            </RouteSelectContainer>
-        );
-    }
+                    Заказать
+                </Button>
+            </FormContainer>
+        </RouteSelectContainer>
+    );
 }
 
 export default compose(
